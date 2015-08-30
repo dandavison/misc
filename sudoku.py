3ed2a01da157f3f79ed2f8bc64f881069d4d8b25
Dan Davison <dan@counsyl.com>
2015-08-30 17:37:33 +0100

    final sudoku

--- sudoku.py
+++ sudoku.py
@@ -5,72 +5,55 @@ import numpy as np
 
 
 class Sudoku(object):
-    def __init__(self, cells):
+
+    def __init__(self, cells, twist):
         self.cells = np.array([[set(range(1, 10)) for i in range(9)]
                                for j in range(9)])
+        self.twist = twist
+
         for index, value in cells.items():
             self.cells[index] = {value}
-        
-    def get_block(self, i, j):
-        rows = [k for k in range(9) if k / 3 == i]
-        columns = [k for k in range(9) if k / 3 == j]
-        row_slice = slice(min(rows), max(rows) + 1)
-        column_slice = slice(min(columns), max(columns) + 1)
-        return self.cells[row_slice, column_slice]
 
-    def check(self):
-        
-        
-        for i in range(3):
-            self.eliminate(self.cells[i, :])
+        self.verbose = False
 
-        for j in range(3):
-            self.eliminate(self.cells[:, j])
-
-        for i in range(3):
-            for j in range(3):
-                self.eliminate(self.get_block(i, j).ravel())
-        
     def solve(self):
 
         it = 0
 
-        hash_0 = self.hash()
+        hash_0 = hash(self)
         while True:
-
-            print it
-            print self
-            print
 
             for i in range(9):
                 self.eliminate(self.cells[i, :])
-    
+
             for j in range(9):
                 self.eliminate(self.cells[:, j])
-    
+
             for i in range(3):
                 for j in range(3):
                     self.eliminate(self.get_block(i, j).ravel())
 
-            if all(len(values) == 1 for values in self.cells.ravel()):
-                print 'solved!'
-                return self
+            self.eliminate(self.get_twist_cells())
 
             it += 1
 
-            hash_1 = self.hash()
+            if all(len(values) == 1 for values in self.cells.ravel()):
+                print 'solved! (iteration %d)' % it
+                return self
+
+            hash_1 = hash(self)
             if hash_0 == hash_1:
-                print self.counts()
-                print self
                 raise RuntimeError("Failed: grid unaltered")
             hash_0 = hash_1
-            
+
             if it == 100:
-                raise RuntimeError("Failed: %d iterations with no solution" % it)
-            
+                raise RuntimeError(
+                    "Failed: %d iterations with no solution" % it)
+
     def eliminate(self, cells):
-        
-        # Find all determined values
+
+        # Find all determined values and create a map from values to possible
+        # cells.
         determined = set()
         values2cells = defaultdict(set)
         for k, values in enumerate(cells):
@@ -81,35 +64,48 @@ class Sudoku(object):
             for value in values:
                 values2cells[value].add(k)
 
+        # Assign values that have only one possible cell
         for value, _cells in values2cells.items():
             if len(_cells) == 1:
-                print "Determined a value by only-possible-cell!"
+                if self.verbose:
+                    print "Determined a value by only-possible-cell!"
                 [cell] = _cells
-                cells[cell] = {value}
-                
-        # Remove them from the other cells
+                assert value in cells[cell]
+                for _value in cells[cell].copy():
+                    if _value != value:
+                        cells[cell].remove(_value)
+
+        # Remove determined values from the set of possibilities in other cells
         for values in cells:
             if len(values) > 1:
                 if determined & values:
-                    # print "Eliminating %s from cell containing %s" % (
-                    #     str(sorted(determined & values)),
-                    #     sorted(values))
                     size = self.size()
                     values -= determined
-                    assert self.size() - size == len(determined & values)
+                    # assert self.size() - size == len(determined & values)
                     if len(values) == 1:
-                        print "Determined a value by only-possible-value!"
-                
+                        if self.verbose:
+                            print "Determined a value by only-possible-value!"
+
+    def get_twist_cells(self):
+        return [self.cells[index] for index in self.twist]
+
+    def get_block(self, i, j):
+        rows = [k for k in range(9) if k / 3 == i]
+        columns = [k for k in range(9) if k / 3 == j]
+        row_slice = slice(min(rows), max(rows) + 1)
+        column_slice = slice(min(columns), max(columns) + 1)
+        return self.cells[row_slice, column_slice]
+
     def counts(self):
         return Counter(map(len, self.cells.ravel()))
 
     def size(self):
-        return sum(self.counts().values())
+        return sum(map(len, self.cells.ravel()))
 
-    def hash(self):
+    def __hash__(self):
         data = tuple(map(tuple, self.cells.ravel()))
         return hash(data)
-    
+
     def __repr__(self):
         chars = ''
         for i in range(9):
@@ -122,49 +118,60 @@ class Sudoku(object):
                 chars += ' '
             chars += '\n'
         return chars
-    
 
 
-if True or __name__ == '__main__':
+
+if __name__ == '__main__':
     cells = {
         (0, 1): 9,
         (0, 2): 1,
         (0, 7): 6,
-    
+
         (1, 0): 8,
         (1, 5): 9,
         (1, 6): 1,
         (1, 8): 4,
-    
+
         (2, 1): 5,
         (2, 4): 1,
         (2, 5): 8,
         (2, 8): 3,
-    
+
         (3, 1): 7,
         (3, 2): 6,
-    
+
         (4, 2): 8,
         (4, 6): 2,
-    
+
         (5, 6): 6,
         (5, 7): 7,
-    
+
         (6, 0): 1,
         (6, 3): 9,
         (6, 4): 3,
         (6, 7): 4,
-    
+
         (7, 0): 3,
         (7, 2): 5,
         (7, 3): 4,
         (7, 8): 7,
-    
+
         (8, 1): 2,
         (8, 6): 3,
         (8, 7): 8,
-        
+
     }
-    sudoku = Sudoku(cells)
+    twist = [
+        (0, 0),
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 3),
+        (5, 4),
+        (6, 5),
+        (7, 4),
+        (8, 5),
+    ]
+    sudoku = Sudoku(cells, twist)
     print sudoku
-    print sudoku.solve()        
+    print sudoku.solve()
