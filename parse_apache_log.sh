629e2d1ea8be3bac4be2703a0d7812617df69313
Dan Davison <dan@counsyl.com>
2015-09-28 21:08:12 -0700

    Parse apache logs

--- parse_apache_log.sh
+++ parse_apache_log.sh
@@ -1,9 +1,19 @@
-ls -1t *_access.log* | head -n 39 | \
+cat_files () {
     while read file ; do
         if [[ $file == *.gz ]] ; then
             gunzip -c $file
         else
             cat $file
         fi
-    done | \
-awk '$9 == 200 {print $7}' | grep '^/' | cut -d? -f1 | sort | uniq -c | sort -rn
+    done
+}
+
+ls -1t *_access.log* | \
+    head -n 39 | \
+    cat_files | \
+    awk '$9 == 200 {print $7}' | \
+    grep '^/' | \
+    cut -d? -f1 | \
+    sort | \
+    uniq -c | \
+    sort -rn
