2594ceb5d0e8528a99349a5582c3f84ef878198e
Dan Davison <dandavison7@gmail.com>
2016-02-25 23:52:00 -0800

    Cleanup

--- getchs_3.py
+++ getchs_3.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
-from time import sleep
 from select import select
+from sys import stdin
+from time import sleep
 import fcntl
 import os
-import sys
-import tty
 import termios
+import tty
+
+
+POLL_INTERVAL = 0.1
 
 
 def setNonBlocking(fd):
     """
     Set the file description of the given file descriptor to non-blocking.
 
-    Copied from twisted source: twisted.internet.fdesc.setNonBlocking
+    Copied from twisted.internet.fdesc.setNonBlocking()
+    https://github.com/twisted/twisted
     """
     flags = fcntl.fcntl(fd, fcntl.F_GETFL)
     flags = flags | os.O_NONBLOCK
@@ -23,16 +27,17 @@ def getchs():
     """
     Block until there are bytes to read on stdin and then return them all.
 
-    Adapted from getch() in https://github.com/joeyespo/py-getch.
+    Adapted from getch()
+    https://github.com/joeyespo/py-getch.
     """
-    fd = sys.stdin.fileno()
+    fd = stdin.fileno()
     old = termios.tcgetattr(fd)
     setNonBlocking(fd)
     try:
         tty.setraw(fd)
-        while not select([fd], [], [], 0)[0]:
-            sleep(0.1)
-        return sys.stdin.read()
+        while not select([stdin], [], [], 0)[0]:
+            sleep(POLL_INTERVAL)
+        return stdin.read()
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old)
 
