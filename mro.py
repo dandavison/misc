6295576b79886b986790ec102eb8577b0ed3703d
Dan Davison <dandavison7@gmail.com>
2016-07-27 18:00:15 -0700

    Add mixin parent

--- mro.py
+++ mro.py
@@ -4,7 +4,12 @@ class Parent(object):
     def __init__(self):
         print 'Parent.__init__'
 
-class Mixin(object):
+class MixinParent(object):
+    def __init__(self):
+        print 'MixinParent.__init__'
+        super(MixinParent, self).__init__()
+
+class Mixin(MixinParent):
     def __init__(self):
         print 'Mixin.__init__'
         super(Mixin, self).__init__()
