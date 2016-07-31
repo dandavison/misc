18bfa1d1be3e9e23b82528fbf3b9551bd5a42bce
Dan Davison <dandavison7@gmail.com>
2016-07-31 11:28:19 -0700

    facebook bookmarklet

---
+++ facebook-make-chat-window-bigger.js
@@ -0,0 +1,2 @@
+// Use "javascript:<code>" in a bookmark
+(function(){var els=document.getElementsByClassName('fbDockChatTabFlyout'); for(var i = 0; i< els.length; i++) {els[i].setAttribute("style", "height:2000px; width:500px")}})()
