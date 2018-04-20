52790e020047335cb3316a97b89842c8b8da9725
Dan Davison <dandavison7@gmail.com>
2018-04-19 22:38:02 -0700

    sympy integration

--- sympy_solve.py
+++ sympy_solve.py
@@ -13,7 +13,12 @@ from sympy import (
 )
 from sympy.plotting import plot_parametric
 
-a, x, y, t, theta = symbols('a x y t theta')
+def make_symbols(*names):
+    globals().update(zip(names, symbols(names)))
+
+
+make_symbols('a', 'b', 'x', 'y', 't', 'theta')
+
 
 eq = Eq(x, t * sp.exp(t))
 solve(eq, t)
@@ -30,3 +35,13 @@ plot_10_2_33 = plot_parametric(
 A1 = integrate(-3 * a**2 * sin(theta)**4 * cos(theta)**2, (theta, pi/2, 0))
 
 A2 = integrate(sqrt() (theta, 0, pi/2))
+
+
+# Oxford M5 1.1.b
+integrate(
+    integrate(
+        (x * y * cos(x**2 * y + y))
+        (x, 0, b),
+    ),
+    (y, 0, a),
+)
