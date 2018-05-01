7e1fac2e3974ccb89d928ed786a2936f124a3b61
Dan Davison <dandavison7@gmail.com>
2018-04-30 23:04:29 -0700

    airbnb.js

---
+++ airbnb.js
@@ -0,0 +1,14 @@
+/* homes */
+
+el = $$('._113wyjr')[0]
+el.setAttribute('style', 'width:10% !important')
+
+/* map */
+
+el = $$('._mhiwnx')[0]
+el.setAttribute('style', 'width:100% !important')
+
+el = $$('._14k7tnhx')[0]
+el.setAttribute('style', 'width:100% !important')
+
+
