3669309d3b124cf45d303ba0d3dc3f7446ace4bc
Dan Davison <dandavison7@gmail.com>
2018-04-30 23:35:07 -0700

    Use thread pool

--- recurrent_async_task_on_tick.py
+++ recurrent_async_task_on_tick.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
-from time import sleep
+from multiprocessing.dummy import Pool as ThreadPool
 from threading import Thread
+from time import sleep
 
 import requests
 
 
 TICKS = 0
 
+
 def clock():
     global TICKS
     while True:
-        sleep(0.5)
+        sleep(2)
         TICKS += 1
 
 
 def do_http_requests_forever():
-    urls = ['https://www.theguardian.com'] * 3
+    urls = ['https://www.theguardian.com'] * 30
     last_satisfied_tick = None
 
     while True:
@@ -25,7 +27,12 @@ def do_http_requests_forever():
             continue
 
         job_tick = TICKS
-        responses = [requests.get(url) for url in urls]
+
+        pool = ThreadPool()
+        responses = pool.map(requests.get, urls)
+        pool.close()
+        pool.join()
+
         if TICKS == job_tick:
             print('%d %s' % (job_tick, [r.status_code for r in responses]))
             last_satisfied_tick = job_tick
