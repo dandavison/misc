28543ef20e24e994e77f319b80a06140810021a8
Dan Davison <dandavison7@gmail.com>
2018-04-30 23:23:01 -0700

    Recurrent async task

--- recurrent_async_task_on_tick.py
+++ recurrent_async_task_on_tick.py
@@ -2,24 +2,37 @@
 from time import sleep
 from threading import Thread
 
+import requests
+
 
 TICKS = 0
 
 def clock():
     global TICKS
     while True:
-        sleep(1)
+        sleep(0.5)
         TICKS += 1
 
 
-def report():
+def do_http_requests_forever():
+    urls = ['https://www.theguardian.com'] * 3
+    last_satisfied_tick = None
+
     while True:
-        sleep(1)
-        print(TICKS)
+
+        if TICKS == last_satisfied_tick:
+            sleep(0.1)
+            continue
+
+        job_tick = TICKS
+        responses = [requests.get(url) for url in urls]
+        if TICKS == job_tick:
+            print('%d %s' % (job_tick, [r.status_code for r in responses]))
+            last_satisfied_tick = job_tick
 
 
 clock_thread = Thread(target=clock)
 clock_thread.start()
 
-reporter_thread = Thread(target=report)
-reporter_thread.start()
+worker_thread = Thread(target=do_http_requests_forever)
+worker_thread.start()
