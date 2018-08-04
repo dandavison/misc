6cf492f8e8fe03bce41f79ba2039e532b12cfd4e
Dan Davison <dandavison7@gmail.com>
2018-08-04 11:46:49 -0700

    utils

--- utils.py
+++ utils.py
@@ -1,21 +1,12 @@
+def format_sql(queryset):
+    import sqlparse
+    sql = str(queryset.values_list('pk').query)
+    return sqlparse.format(sql, reindent=True)
+
+
 def execute_sql(sql):
     if not sql.rstrip().endswith(';'):
         sql = sql + ';'
-    c = connection.cursor()
-    c.execute(sql)
-    return c.fetchall()
-
-
-def format_sql(sql):
-    import sqlparse
-    return sqlparse.format(unicode(sql), reindent=True)
-
-
-def format_queryset_sql(qs):
-    return format_sql(qs.values_list('pk').query)
-
-
-def execute_sql(sql):
     c = connection.cursor()
     c.execute(sql)
     return c.fetchall()
@@ -438,3 +429,96 @@ def print_git_diff(diff):
             print(green(line))
         else:
             print(line)
+
+
+from contextlib import contextmanager
+from django.db import connection
+from django.db import transaction
+
+
+def max_queries(n, trace=True):
+    from functools import wraps
+    from django.db import connection
+    from clint.textui.colored import red as col
+
+    def _max_queries(func):
+
+        def __max_queries(*args, **kwargs):
+            q0 = len(connection.queries)
+            ret = func(*args, **kwargs)
+            q1 = len(connection.queries)
+            if trace:
+                print col("%s: %d queries" % (func.__name__, q1 - q0))
+            if q1 - q0 > n:
+                raise AssertionError("%d queries in %s" % (
+                    q1 - q0,
+                    func.__name__,
+                ))
+            return ret
+
+        return wraps(func)(__max_queries)
+
+    return _max_queries
+
+
+def print_queries(func):
+    from functools import wraps
+    from django.db import connection
+    from clint.textui.colored import green as col
+
+    def _print_queries(*args, **kwargs):
+        q0 = len(connection.queries)
+        ret = func(*args, **kwargs)
+        q1 = len(connection.queries)
+        print col("%s: %d queries" % (func.__name__, q1 - q0))
+        return ret
+
+    return wraps(func)(_print_queries)
+
+
+@contextmanager
+def rollback():
+    """
+    Run statements in a transaction and do not commit.
+    """
+    if transaction.is_managed():
+        raise RuntimeError("May not be run in a transaction")
+    class Rollback(Exception):
+        pass
+    try:
+        with transaction.commit_on_success():
+            yield
+            raise Rollback()
+    except Rollback:
+        pass
+
+
+def log(func):
+    from functools import wraps
+    from django.db import connection
+
+    def _log(*args, **kwargs):
+        from clint.textui import colored; blue = lambda s: colored.blue(s, bold=True)
+        print(blue(func.__name__))
+        return func(*args, **kwargs)
+
+    return wraps(func)(_log)
+
+
+def trace(func):
+    from functools import wraps
+    from django.db import connection
+
+    def _print_queries(*args, **kwargs):
+        q0 = len(connection.queries)
+        ret = func(*args, **kwargs)
+        q1 = len(connection.queries)
+        print "%s(%s, %s): %d queries" % (
+            func.__name__,
+            args,
+            kwargs,
+            q1 - q0,
+        )
+        return ret
+
+    return wraps(func)(_print_queries)
