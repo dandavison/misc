65b906845a4eff7994c3847044b356f7d5f222e5
Dan Davison <dandavison7@gmail.com>
2018-12-06 21:46:05 -0500

    Use reduce/cons

--- aoc-2018-5.py
+++ aoc-2018-5.py
@@ -1,4 +1,4 @@
-import sys
+from functools import reduce
 
 
 def part1(polymer):
@@ -6,27 +6,20 @@ def part1(polymer):
 
 
 def react(polymer):
-    while True:
-        i = get_index_of_reactive_pair(polymer)
-        if i is None:
-            return polymer
-        else:
-            polymer = polymer[:i] + polymer[i+2:]
+    return reduce(cons, list(polymer), [])
 
 
-def get_index_of_reactive_pair(polymer):
-    assert len(polymer) > 1
-    prev = polymer[0]
-    for i in range(1, len(polymer)):
-        curr = polymer[i]
-        if prev.lower() == curr.lower():
-            if (prev.islower() and curr.isupper() or
-                prev.isupper() and curr.islower()):
-                return i - 1
-        prev = curr
-    return None
+def cons(xs, y):
+    if not xs:
+        return [y]
+    *xs, x = xs
+    if x.lower() == y.lower() and x != y:
+        return xs
+    else:
+        return xs + [x] + [y]
 
 
 if __name__ == '__main__':
+    import sys
     polymer = sys.stdin.read().rstrip()
     print(part1(polymer))
