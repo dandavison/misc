f6177f8d7fcee666816d93f31e873841eaec3c7b
Dan Davison <dandavison7@gmail.com>
2018-12-09 16:09:54 -0500

    aoc 7: WIP schedule tasks

--- aoc-2018-7.py
+++ aoc-2018-7.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from itertools import chain
+from string import ascii_uppercase
 
 
 def read_edges(fp):
@@ -32,8 +33,84 @@ def topological_sort(graph):
             del graph[next_node]
 
 
+class Worker:
+    def __init__(self, label):
+        self.label = label
+        self.time2task = {}
+
+    def __str__(self):
+        return f'Worker({self.label})'
+
+    def is_available(self, time):
+        return time not in self.time2task
+
+
+class Scheduler:
+    def __init__(self, dependency_graph, n_workers, base_time):
+        self.dependency_graph = dependency_graph.copy()
+        self.workers = [Worker(i) for i in range(n_workers)]
+        self.base_time = base_time
+        self.current_time = 1
+
+    @property
+    def tasks(self):
+        return self.dependency_graph.keys()
+
+    @property
+    def tasks_without_dependencies(self):
+        return self.tasks - self.tasks_with_dependecies
+
+    @property
+    def tasks_with_dependecies(self):
+        return set(chain.from_iterable(self.dependency_graph.values()))
+
+    @property
+    def final_task_end_time(self):
+        return max(chain.from_iterable(w.time2task.keys() for w in self.workers))
+
+    @property
+    def available_workers(self):
+        return [w for w in self.workers if w.is_available(self.current_time)]
+
+    def consume_ready_tasks(self):
+        min_end_time = 0xFFFFFFFF
+
+        print(f'At time {self.current_time} available workers are {[str(w) for w in self.available_workers]}')
+
+        for task, worker in zip(self.tasks_without_dependencies, self.available_workers):
+
+            from clint.textui import colored; red = lambda s: colored.red(s, bold=True)
+            print(red(f'Assigning task {task} to worker {worker} at time {self.current_time}'))
+
+            end_time = self.current_time + self.task_duration(task)
+            min_end_time = min(min_end_time, end_time)
+            for t in range(self.current_time, end_time):
+                worker.time2task[t] = task
+            del self.dependency_graph[task]
+        self.current_time = min_end_time
+
+        for worker in self.workers:
+            print(worker.time2task)
+
+
+    def task_duration(self, task):
+        return self.base_time + 1 + ascii_uppercase.index(task)
+
+
+def total_time(dependency_graph, n_workers, base_time):
+    scheduler = Scheduler(dependency_graph, n_workers, base_time)
+    while True:
+        if not scheduler.tasks_without_dependencies:
+            return scheduler.final_task_end_time
+        else:
+            scheduler.consume_ready_tasks()
+
+
 from sys import stdin
-graph = make_adjacency_list(read_edges(stdin))
+fp = open('/tmp/7.txt')
+graph = make_adjacency_list(read_edges(fp))
 print(graph)
 # part 1
 print(topological_sort(graph))
+# part 2
+print(total_time(graph, n_workers=2, base_time=0))
