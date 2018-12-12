ad28ae985436af183597f99d4e811e1830077407
Dan Davison <dandavison7@gmail.com>
2018-12-12 12:06:19 -0500

    aoc 11 part 2

--- aoc_2018_11.py
+++ aoc_2018_11.py
@@ -1,4 +1,34 @@
+import operator
+from collections import Counter
+from functools import reduce
+from math import sqrt
+
 import numpy as np
+
+
+def factorize(n):
+    factorized = {}
+
+    def _factorize(n):
+        assert n > 0
+        if n in factorized:
+            return factorized[n]
+
+        if n < 4:
+            factors = Counter([n])
+        else:
+            for i in range(int(sqrt(n)), 2 - 1, -1):
+                quot, rem = divmod(n, i)
+                if not rem:
+                    return factorize(i) + factorize(quot)
+            else:
+                factors = Counter([n])
+
+        factorized[n] = factors
+        return factors
+
+    return _factorize(n)
+
 
 def get_power_level(x, y, serial_number):
     """
@@ -6,7 +36,8 @@ def get_power_level(x, y, serial_number):
     - Begin with a power level of the rack ID times the Y coordinate.
     - Increase the power level by the value of the grid serial number (your puzzle input).
     - Set the power level to itself multiplied by the rack ID.
-    - Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
+    - Keep only the hundreds digit of the power level
+      (so 12345 becomes 3; numbers with no hundreds digit become 0).
     - Subtract 5 from the power level.
 
     (3, 5)
@@ -24,47 +55,59 @@ def get_power_level(x, y, serial_number):
     return power_level
 
 
-def convolve2d(image, kernel):
-    # http://machinelearninguru.com/computer_vision/basics/convolution/image_convolution_1.html
-    # This function which takes an image and a kernel
-    # and returns the convolution of them
-    # Args:
-    #   image: a numpy array of size [image_height, image_width].
-    #   kernel: a numpy array of size [kernel_height, kernel_width].
-    # Returns:
-    #   a numpy array of size [image_height, image_width] (convolution output).
-    output = np.zeros_like(image)            # convolution output
-    image_padded = np.zeros((image.shape[0] + 2, image.shape[1] + 2))
-    image_padded[:] = -np.inf
-    image_padded[1:-1, 1:-1] = image
-    for x in range(image.shape[1]):
-        for y in range(image.shape[0]):
-            output[y, x] = (kernel * image_padded[y:y+3, x:x+3]).sum()
-    return output
-
-
-
 def get_array(serial_number):
-    array = np.zeros(shape=(300,300))
+    array = np.zeros(shape=(300, 300))
     for i in range(300):
         for j in range(300):
             array[i, j] = get_power_level(i, j, serial_number)
     return array
 
 
-def get_max_subsquare(size):
-    kernel = np.ones(shape=(size, size))
-    convolved = convolve2d(ARRAY, kernel)
-    xmax, ymax = np.unravel_index(np.argmax(convolved), dims=ARRAY.shape)
-    return xmax, ymax, convolved[xmax, ymax]
+SERIAL_NUMBER = 9445
+SQUARE = get_array(SERIAL_NUMBER)
+_CACHE = {}
+
+
+def memoized(f):
+    def inner(*args):
+        if args not in _CACHE:
+            _CACHE[args] = f(*args)
+        return _CACHE[args]
+    return inner
+
+
+@memoized
+def get_subsquare_sum(i, j, size):
+    factorization = factorize(size)
+    if len(factorization) == 1:
+        return SQUARE[i:(i + size), j:(j + size)].sum()
+    else:
+        (largest_factor, largest_factor_exp), *others = sorted(factorization.items(), reverse=True)
+        multiplicity = (largest_factor ** (largest_factor_exp - 1)
+                        * reduce(operator.mul, (fac ** exp for fac, exp in others), 1))
+        return sum(get_subsquare_sum(i + largest_factor * k,
+                                     j + largest_factor * l,
+                                     largest_factor)
+                   for k in range(multiplicity)
+                   for l in range(multiplicity))
+
+
+def get_max_subsquare_sum(size):
+    [square_size] = set(SQUARE.shape)
+    max_sum, max_coord = max((get_subsquare_sum(i, j, size), (i, j))
+                             for i in range(square_size - size + 1)
+                             for j in range(square_size - size + 1))
+    return max_coord
 
 
 def part1():
-    xmax, ymax, _ = get_max_subsquare(3)
-    return xmax - 1, ymax - 1
+    return get_max_subsquare_sum(3)
 
 
-if __name__ == '__main__':
-    SERIAL_NUMBER = 9445
-    ARRAY = get_array(SERIAL_NUMBER)
-    print(part1())
+def part2():
+    [square_size] = set(SQUARE.shape)
+    max_sum, max_coord = max((get_subsquare_sum(i, j, size), (i, j, size))
+                             for size in range(1, square_size)
+                             for i in range(square_size - size + 1)
+                             for j in range(square_size - size + 1))
+    return max_coord
