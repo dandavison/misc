158e4d14804e57a01d71ae5b1e490f99809f0798
Dan Davison <dandavison7@gmail.com>
2018-12-13 19:31:27 -0500

    WIP

--- aoc_2018_13.py
+++ aoc_2018_13.py
@@ -11,21 +11,39 @@ def parse_input(fp):
 
 
 SYMBOL_2_DIRECTION = {
-    '^': +1 + 0j,
+    'v': +1 + 0j,
     '>': +0 + 1j,
-    'v': -1 + 0j,
+    '^': -1 + 0j,
     '<': +0 - 1j,
 }
-
-TURNS = [1j, 1, -1j]
-
 
 @dataclass
 class Cart:
     location: complex
     direction: complex
-    n_turns: int=0
 
+    INTERSECTION_DIRECTIONS = [1j, 1, -1j]
+    n_intersections: int = 0
+
+    @property
+    def next_intersection_direction(self):
+        return self.INTERSECTION_DIRECTIONS[self.n_intersections % 3]
+
+    def get_turn_direction(self, track):
+        """
+        |    | \  | /  |
+        |----+----+----|
+        |  1 | -j | j  |
+        |  j | j  | -j |
+        | -1 | -j | j  |
+        | -j | j  | -j |
+        """
+        return {
+            '\\': {1: -1j, 1j: 1j, -1: -1j, -1j: 1j},
+            '/': {1: 1j, 1j: -1j, -1: 1j, -1j: -1j},
+        }[track][self.direction]
+
+    @property
     def track_symbol(self):
         """
         The track symbol that would be present if the cart symbol wasn't there.
@@ -33,9 +51,18 @@ class Cart:
         return '|' if self.direction.real else '-'
 
 
-@dataclass
 class State:
-    array: np.array
+
+    def __init__(self, array, carts=None):
+        self.array = array
+        self.carts = self._get_carts() if carts is None else carts
+        for cart in self.carts:
+            self[cart.location] = cart.track_symbol
+
+    def _get_carts(self):
+        return sorted((Cart(location, SYMBOL_2_DIRECTION[self[location]])
+                       for location in self.cart_locations),
+                      key=lambda c: (c.location.real, c.location.imag))
 
     @property
     def cart_locations(self):
@@ -53,33 +80,23 @@ class State:
         return int(index.real), int(index.imag)
 
     def print(self):
-        print()
-        print('\n'.join(''.join(row) for row in self.array))
-        print()
+        self = State(self.array.copy(), self.carts)
+        for cart in self.carts:
+            self[cart.location] = invert_dict(SYMBOL_2_DIRECTION)[cart.direction]
+        print('\n' + '\n'.join(''.join(row) for row in self.array) + '\n')
 
 
 def evolve(state, n_generations):
-    carts = []
-
-    for location in state.cart_locations:
-        direction = SYMBOL_2_DIRECTION[state[location]]
-        carts.append(Cart(location, direction))
-
-    carts.sort(key=lambda c: (c.location.real, c.location.imag))
-
     for gen in range(n_generations):
-        for cart in carts:
-            state[cart.location] = '|' if cart.location.real else '-'
-
-            print(f'cart at {cart.location} has track {state[cart.location]} and direction {cart.direction}')
-
+        for cart in state.carts:
+            print(f'cart at {cart.location} has track {cart.track_symbol}, direction {cart.direction}, turn {cart.n_intersections}')
             cart.location += cart.direction
-            if state[cart.location] == '+':
-                cart.direction *= TURNS[cart.n_turns % 3]
-                cart.location += cart.direction
-                cart.n_turns += 1
-            state[cart.location] = invert_dict(SYMBOL_2_DIRECTION)[cart.direction]
-
+            track = state[cart.location]
+            if track == '+':
+                cart.direction *= cart.next_intersection_direction
+                cart.n_intersections += 1
+            elif track in '\/':
+                cart.direction *= cart.get_turn_direction(track)
         state.print()
 
 
@@ -91,4 +108,4 @@ with open('/tmp/13.txt') as fp:
     state = State(parse_input(fp))
 
 state.print()
-evolve(state, 2)
+evolve(state, 3)
