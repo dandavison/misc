e93e952a325944def36da429090d89ad140180f2
Dan Davison <dandavison7@gmail.com>
2019-10-10 19:54:23 -0700

    Change variable names

--- 3blue1brown-blocks.py
+++ 3blue1brown-blocks.py
@@ -4,53 +4,43 @@ import sys
 from dataclasses import dataclass
 from typing import Tuple
 
-VERBOSE = False
-
 
 @dataclass
 class Blocks:
-    m0 = 1.0  # mass of left block
-    m1 = None  # mass of right block (read from argv)
+    m1 = 1.0  # mass of left block
+    m2 = None  # mass of right block (supplied as input)
 
-    v0 = 0.0  # left block starts off stationary
-
-    v1 = -1.0  # initial velocity of right block
+    v1 = 0.0  # left block starts off stationary
+    v2 = -1.0  # initial velocity of right block
 
     def will_collide_again(self):
-        return abs(self.v0) > self.v1
+        return abs(self.v1) > self.v2
 
     def collide(self):
-        v0, v1 = self.v0, self.v1
-        m0, m1 = self.m0, self.m1
-        self.v0 = (m0 * v0 - m1 * v0 + 2 * m1 * v1) / (m0 + m1)
-        self.v1 = (2 * m0 * v0 - m0 * v1 + m1 * v1) / (m0 + m1)
+        v1, v2 = self.v1, self.v2
+        m1, m2 = self.m1, self.m2
+        self.v1 = (2 * m2 * v2 - v1 * (m2 - m1)) / (m1 + m2)
+        self.v2 = (2 * m1 * v1 + v2 * (m2 - m1)) / (m1 + m2)
 
-    def print(self):
-        if VERBOSE:
-            print(f"{self.v0:.2f} {self.v1:.2f}")
+    def simulate(self):
+        n = 0
+        while True:
+            self.collide()
+            n += 1
+            if self.will_collide_again():
+                # Bounce off wall
+                self.v1 = -self.v1
+                n += 1
+            else:
+                break
+        if self.v1 < 0:
+            # Bounce off wall
+            n += 1
+        print(n)
 
 
 if __name__ == "__main__":
     assert not sys.argv[2:]
-
     blocks = Blocks()
-    blocks.m1 = float(sys.argv[1])
-
-    n = 0
-    while True:
-        blocks.print()
-        blocks.collide()
-        blocks.print()
-        n += 1
-        if blocks.will_collide_again():
-            # Bounce off wall
-            blocks.v0 = -blocks.v0
-            n += 1
-        else:
-            break
-
-    if blocks.v0 < 0:
-        # Bounce off wall
-        n += 1
-
-    print(n)
+    blocks.m2 = float(sys.argv[1])
+    blocks.simulate()
