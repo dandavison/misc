c74fc9d4b531a5192cc076a8c0c448ae7bedcf39
Dan Davison <dandavison7@gmail.com>
2019-10-12 15:47:54 -0700

    Switch variables for consistency with video

--- 3blue1brown-blocks.py
+++ 3blue1brown-blocks.py
@@ -2,25 +2,24 @@
 # https://www.youtube.com/watch?v=HEfHFsfGXjs
 import sys
 from dataclasses import dataclass
-from typing import Tuple
 
 
 @dataclass
 class Blocks:
-    m1 = 1.0  # mass of left block
-    m2 = None  # mass of right block (supplied as input)
+    m1 = None  # mass of right block (supplied as input)
+    m2 = 1.0  # mass of left block
 
-    v1 = 0.0  # left block starts off stationary
-    v2 = -1.0  # initial velocity of right block
+    v1 = -1.0  # initial velocity of right block
+    v2 = 0.0  # left block starts off stationary
 
     def will_collide_again(self):
-        return abs(self.v1) > self.v2
+        return abs(self.v2) > self.v1
 
     def collide(self):
         v1, v2 = self.v1, self.v2
         m1, m2 = self.m1, self.m2
-        self.v1 = (2 * m2 * v2 - v1 * (m2 - m1)) / (m1 + m2)
-        self.v2 = (2 * m1 * v1 + v2 * (m2 - m1)) / (m1 + m2)
+        self.v1 = (2 * m2 * v2 + v1 * (m1 - m2)) / (m2 + m1)
+        self.v2 = (2 * m1 * v1 - v2 * (m1 - m2)) / (m2 + m1)
 
     def simulate(self):
         n = 0
@@ -29,11 +28,11 @@ class Blocks:
             n += 1
             if self.will_collide_again():
                 # Bounce off wall
-                self.v1 = -self.v1
+                self.v2 = -self.v2
                 n += 1
             else:
                 break
-        if self.v1 < 0:
+        if self.v2 < 0:
             # Bounce off wall
             n += 1
         print(n)
@@ -42,5 +41,5 @@ class Blocks:
 if __name__ == "__main__":
     assert not sys.argv[2:]
     blocks = Blocks()
-    blocks.m2 = float(sys.argv[1])
+    blocks.m1 = float(sys.argv[1])
     blocks.simulate()
