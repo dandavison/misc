6a6e8bceb0aef398f5e61421aec72602eec737f6
Dan Davison <dandavison7@gmail.com>
2019-11-07 07:20:55 -0600

    Poisson process / exponential rate estimates
    Fix simulation

--- poisson_process.py
+++ poisson_process.py
@@ -32,11 +32,17 @@ def simulate_and_estimate_bounded_2(rate, n):
     number_of_sampled_event_streams = int(n)
     all_times = exponential(scale=1 / rate, size=number_of_sampled_event_streams)
     halfway_point = int(number_of_sampled_event_streams / 3)
-    times_to_first_event = all_times[:halfway_point]
-    bounded_times = all_times[halfway_point:]
+    times_to_first_event = list(all_times[:halfway_point])
+    potentially_bounded_times = list(all_times[halfway_point:])
+    bounded_times = []
+    for t in potentially_bounded_times:
+        if t > rate:
+            bounded_times.append(rate)
+        else:
+            times_to_first_event.append(t)
     n_T = len(times_to_first_event)
     n_B = len(bounded_times)
-    rate_estimate = n_T / (times_to_first_event.sum() + bounded_times.sum())
+    rate_estimate = n_T / (sum(times_to_first_event) + sum(bounded_times))
     print(f"n_T={n_T}, n_B={n_B}, rate_estimate={rate_estimate}")
 
 
