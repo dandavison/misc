1a3f69ccc0c9596407ae1fd6489bf152b2163f93
Dan Davison <dandavison7@gmail.com>
2019-11-07 07:15:43 -0600

    Poisson process / exponential rate estimates
    Incorrect simulation

--- poisson_process.py
+++ poisson_process.py
@@ -28,5 +28,17 @@ def simulate_and_estimate_bounded(rate, time_bound, n):
     print(f"n_T={n_T}, n_B={n_B}, rate_estimate={rate_estimate}")
 
 
+def simulate_and_estimate_bounded_2(rate, n):
+    number_of_sampled_event_streams = int(n)
+    all_times = exponential(scale=1 / rate, size=number_of_sampled_event_streams)
+    halfway_point = int(number_of_sampled_event_streams / 3)
+    times_to_first_event = all_times[:halfway_point]
+    bounded_times = all_times[halfway_point:]
+    n_T = len(times_to_first_event)
+    n_B = len(bounded_times)
+    rate_estimate = n_T / (times_to_first_event.sum() + bounded_times.sum())
+    print(f"n_T={n_T}, n_B={n_B}, rate_estimate={rate_estimate}")
+
+
 if __name__ == "__main__":
     simulate_and_estimate_bounded(rate=3.749)
