3321a7a62900312d9664ab9f1589ba87c4ee47b8
Dan Davison <dandavison7@gmail.com>
2019-11-23 12:47:08 -0500

    Trace executables

--- dvipng-wrapper.sh
+++ dvipng-wrapper.sh
@@ -1,4 +1,4 @@
-log=/tmp/dvisvgm.log
+log=/tmp/dvipng.log
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . /Users/dan/src/misc/trace.sh
-exec /usr/local/texlive/2018/bin/x86_64-darwin/dvisvgm $@
+exec /usr/local/texlive/2018/bin/x86_64-darwin/dvipng $@
---
+++ trace.sh
@@ -0,0 +1,7 @@
+date >> $log
+echo '----------------------------' >> $log
+echo '$PWD: ' $PWD >> $log
+echo '$(pwd): ' $(pwd) >> $log
+echo '$0: ' $0 >> $log
+echo '$@: ' $@ >> $log
+echo >> $log
