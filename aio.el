a8f3e6cc33b7b719c92438c76b40a4dda4d9a9c0
Dan Davison <dandavison7@gmail.com>
2019-12-22 20:56:01 +0000

    aio examples

---
+++ aio.el
@@ -0,0 +1,42 @@
+#!/usr/bin/env emacs --script
+(add-to-list 'load-path "~/src/3p/emacs-aio")
+(require 'aio)
+
+(setq lexical-binding t)
+
+(defun make-promise:call-function (seconds function)
+  (let ((promise (aio-promise))
+        (value-function (lambda () (assert lexical-binding) (funcall function))))
+    (aio-listen promise (lambda (value-function) (funcall value-function)))
+    (prog1 promise
+      (run-at-time seconds nil #'aio-resolve promise value-function))))
+
+(defun example-1 ()
+  "Execute a function in the future."
+  (message "starting example-1")
+  (make-promise:call-function 1 (lambda () (message "example-1 callback"))))
+
+(aio-defun example-2 ()
+  "Execute two functions in the future."
+  (aio-await (make-promise:call-function 3 (lambda () (message "example-2 callback 1"))))
+  (aio-await (make-promise:call-function 1 (lambda () (message "example-2 callback 2")))))
+
+(aio-defun example-3 ()
+  "Run a child process asynchronously."
+  (let ((sentinel-file (format "/tmp/example-3-%s" (format-time-string "%s%3N%6N"))))
+    (make-process
+     :command `("bash" "-c" ,(format "sleep 2 && touch %s" sentinel-file))
+     :sentinel (lambda () (message "example-3 callback")))))
+
+(defun run (fn)
+  (with-current-buffer "*Messages*"
+    (setq buffer-read-only nil)
+    (goto-char (point-max))
+    (insert "\n\n\n\n"))
+  (funcall fn)
+  (message "started %s" (symbol-name fn))
+  (message ""))
+
+(when nil
+  (run 'example-3)
+  )
