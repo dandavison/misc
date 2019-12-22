73dfccfe87de334cc93780f68fd06df2d4c444c3
Dan Davison <dandavison7@gmail.com>
2019-12-22 23:25:23 +0000

    aio: chaining processes

--- aio.el
+++ aio.el
@@ -1,7 +1,7 @@
 #!/usr/bin/env emacs --script
-(add-to-list 'load-path "~/src/3p/emacs-aio")
 (setq-default lexical-binding t)
 (setq lexical-binding t)
+(add-to-list 'load-path "~/src/3p/emacs-aio")
 (require 'aio)
 
 (defun make-promise:call-function (seconds function)
@@ -21,13 +21,35 @@
   (aio-await (make-promise:call-function 3 (lambda () (message "example-2 callback 1"))))
   (aio-await (make-promise:call-function 1 (lambda () (message "example-2 callback 2")))))
 
+(defun make-process-demo ()
+  (make-process
+   :name "example-3"
+   :command '("bash-wrapper" "-c" "ls -l")
+   :sentinel (lambda (process event) (message "example-3 callback"))))
+
 (defun example-3 ()
   "Run a child process asynchronously."
   (let ((sentinel-file (format "/tmp/example-3-%s" (format-time-string "%s%3N%6N"))))
-    (make-process
-     :name "example-3"
-     :command '("bash-wrapper" "-c" "ls -l")
-     :sentinel (lambda (process event) (message "example-3 callback")))))
+    (make-process-demo)))
+
+(defun make-promise:make-process (callback)
+  (let ((promise (aio-promise))
+        (value-function callback))
+    (aio-listen promise (lambda (value-function) (funcall value-function)))
+    (prog1 promise
+      (make-process
+       :name "make-promise:make-process--process-name"
+       :command '("/Users/dan/bin/bash-wrapper" "-c" "ls -l")
+       :sentinel (lambda (process event) (message "sentinel") (aio-resolve promise value-function))))))
+
+(aio-defun example-4 ()
+  "Run a child processes asynchronously, using a promise"
+  (make-promise:make-process (lambda () (message "example-4 callback 1"))))
+
+(aio-defun example-5 ()
+  "Run two child processes asynchronously, one after the other"
+  (aio-await (make-promise:make-process (lambda () (message "example-5 callback 1"))))
+  (aio-await (make-promise:make-process (lambda () (message "example-5 callback 2")))))
 
 (defun run (fn)
   (with-current-buffer "*Messages*"
@@ -38,6 +60,9 @@
   (message "started %s" (symbol-name fn))
   (message ""))
 
-(when nil
+(when (or noninteractive nil)
   (run 'example-3)
   )
+
+(if noninteractive
+    (sleep-for 5))
