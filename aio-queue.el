2134493381379ce0140a48b5376a8024a9645daa
Dan Davison <dandavison7@gmail.com>
2020-01-17 10:20:50 -0500

    emacs-aio: run tasks asynchronously

--- aio-queue.el
+++ aio-queue.el
@@ -3,10 +3,20 @@
 (add-to-list 'load-path "~/src/3p/emacs-aio")
 (require 'aio)
 
-(defun task (n)
-  (message "starting task %s" n)
-  (sleep-for 1)
-  (message "finished task %s" n))
+(defun runif ()
+  (/ (random 100) 100.0))
 
-(dolist (n '(1 2 3 4 5 6 7 8))
-  (task n))
+(aio-defun task (n)
+  (message "starting %s" n)
+  (aio-await (aio-sleep (runif)))
+  (message "checkpoint %s" n)
+  (message "finished %s" n))
+
+(setq all-tasks-promise
+      (aio-all (mapcar #'task '(1 2 3 4 5 6 7 8))))
+
+(dolist (n (number-sequence 1 20))
+  (message "main %s" n)
+  (sleep-for 0.1))
+
+(aio-wait-for all-tasks-promise)
