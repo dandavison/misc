64e714426408d142a557c41dccef4e35a44a0eca
Dan Davison <dandavison7@gmail.com>
2020-02-28 10:09:48 -0600

    dask cloud provider

--- dask_bag.py
+++ dask_bag.py
@@ -1,13 +1,25 @@
 import subprocess
 
 import dask.bag as dask_bag
+from dask_cloudprovider import ECSCluster
+from dask_cloudprovider import FargateCluster
+from dask.distributed import Client
 
-if False:
-    from dask_cloudprovider import FargateCluster
-    from dask.distributed import Client
 
-    cluster = FargateCluster(n_workers=1)
+# https://medium.com/rapids-ai/getting-started-with-rapids-on-aws-ecs-using-dask-cloud-provider-b1adfdbc9c6e
+def activate_dask_cluster():
+    if True:
+        cluster = FargateCluster(n_workers=1)
+    else:
+        cluster = ECSCluster(
+            cluster_arn="arn:aws:ecs:us-west-2:169597880835:cluster/sylph",
+            n_workers=1,
+            fargate_scheduler=True,
+        )
+
     client = Client(cluster)
+    print(cluster)
+    print(cluster.dashboard_link)
 
 
 def process(args):
@@ -19,6 +31,9 @@ def f(i):
 
 
 if __name__ == "__main__":
+    if True:
+        activate_dask_cluster()
+
     bag = dask_bag.from_sequence(range(10))
     z = bag.map(f)
     for result in z.compute():
