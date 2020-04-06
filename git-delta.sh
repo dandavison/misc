095adac99772ca93a511bff4edd13464c1b88d34
Dan Davison <dandavison7@gmail.com>
2020-04-06 05:40:26 -0400

    Improve git-delta script

--- git-delta.sh
+++ git-delta.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-set -euo pipefail
-
-git $@ | delta --24-bit-color always --theme GitHub --width variable --retain-plus-minus-markers --commit-style plain --file-style plain --hunk-style plain
+if $(grep -qE "( show | diff )" <<< "$@"); then
+    handler=delta
+    set -o pipefail
+    git $@ | delta --24-bit-color always --theme GitHub --width variable --retain-plus-minus-markers --commit-style plain --file-style plain --hunk-style plain
+else
+    handler=git
+    git $@
+fi
 
 exit_status=$?
 
-echo $exit_status $0 $@ >> /tmp/git-delta.log
+echo $exit_status $handler $@ >> /tmp/git-delta.log
 
 exit $exit_status
