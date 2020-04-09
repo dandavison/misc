4dbe3ee3c9a86bb419fdd1cfe0cdb1b90dc15ac3
Dan Davison <dandavison7@gmail.com>
2020-04-09 00:10:04 -0400

    Updates and fixes to git-delta wrapper

--- git-delta.sh
+++ git-delta.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+set -o pipefail
 
 if $(grep -qE "( show | diff )" <<< "$@"); then
-    handler=delta
-    set -o pipefail
-    git $@ | delta --24-bit-color always --theme GitHub --width variable --retain-plus-minus-markers --commit-style plain --file-style plain --hunk-style plain
+    git "$@" | delta --24-bit-color always \
+                     --theme GitHub \
+                     --width variable \
+                     --retain-plus-minus-markers \
+                     --commit-style plain \
+                     --file-style plain \
+                     --hunk-style plain \
+                     --max-line-distance=0.6
 else
-    handler=git
-    git $@
+    git "$@"
 fi
 
 exit_status=$?
 
-echo $exit_status $handler $@ >> /tmp/git-delta.log
+echo "$@" >> /tmp/git-delta.log
 
 exit $exit_status
