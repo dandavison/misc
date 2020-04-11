55943b4a236ce1f4893a8feddc357b1958907454
Dan Davison <dandavison7@gmail.com>
2020-04-11 19:30:27 -0400

    Update git-delta

--- git-delta.sh
+++ git-delta.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-set -o pipefail
-
-if $(grep -qE "( show | diff )" <<< "$@"); then
-    git "$@" | delta --24-bit-color always \
-                     --theme GitHub \
-                     --width variable \
-                     --retain-plus-minus-markers \
-                     --commit-style plain \
-                     --file-style plain \
-                     --hunk-style plain \
-                     --max-line-distance=0.6
-else
-    git "$@"
-fi
-
-exit_status=$?
 
 echo "$@" >> /tmp/git-delta.log
 
-exit $exit_status
+set -o pipefail
+
+DELTA=~/.facet/facets/magit-delta/delta
+
+if $(grep -qE "( show | diff )" <<< "$@"); then
+    git "$@" | $DELTA \
+                   --24-bit-color always \
+                   --theme GitHub \
+                   --max-line-distance=0.6 \
+                   --tabs 0 \
+                   --width variable \
+                   --retain-plus-minus-markers \
+                   --commit-style plain \
+                   --file-style plain \
+                   --hunk-style plain
+else
+    git "$@"
+fi
