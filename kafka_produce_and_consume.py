5448ac9835508e492087e27882e240ef737a13be
Dan Davison <dandavison7@gmail.com>
2020-08-24 12:17:16 -0400

    .

--- kafka_produce_and_consume.py
+++ kafka_produce_and_consume.py
@@ -11,7 +11,7 @@ def check_connection(server):
     return sock.connect((host, int(port)))
 
 # E.g.
-# export KAFKA_SERVERS='b-2.lims-staging.x7e7yl.c3.kafka.us-west-2.amazonaws.com:9094 b-1.lims-staging.x7e7yl.c3.kafka.us-west-2.amazonaws.com:9094 b-3.lims-staging.x7e7yl.c3.kafka.us-west-2.amazonaws.com:9094' 
+# export KAFKA_SERVERS='b-2.lims-staging.x7e7yl.c3.kafka.us-west-2.amazonaws.com:9094 b-1.lims-staging.x7e7yl.c3.kafka.us-west-2.amazonaws.com:9094 b-3.lims-staging.x7e7yl.c3.kafka.us-west-2.amazonaws.com:9094'
 SERVERS = [s.strip() for s in os.environ["KAFKA_SERVERS"].split()]
 
 print("Checking connections to servers...", end="")
@@ -27,6 +27,21 @@ consumer = KafkaConsumer(**config)
 
 print("Producing")
 producer.send("dan-temp-topic", "dan-message=1")
+consumer.subscribe(["dan-temp-topic"])
 print("Consuming")
 for i in range(10):
     print(consumer.poll(1000, 1))
+
+# Checking connections to servers...OK
+# Producing
+# Consuming
+# {}
+# {}
+# {}
+# {}
+# {}
+# {}
+# {}
+# {}
+# {}
+# {}
