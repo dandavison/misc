9ec1030e4ba1595087d7c12b46bff2b0f2e12224
Dan Davison <dandavison7@gmail.com>
2020-08-24 19:01:18 -0400

    Consume in a thread

--- kafka_produce_and_consume.py
+++ kafka_produce_and_consume.py
@@ -1,5 +1,6 @@
 import os
 import socket
+from threading import Thread
 
 SERVERS = [s.strip() for s in os.environ["KAFKA_SERVERS"].split()]
 TOPIC = "dan-temp-topic"
@@ -18,13 +19,14 @@ print("OK")
 config = dict(bootstrap_servers=SERVERS,
               security_protocol="SSL")
 
-# Producer
-from kafka import KafkaProducer
-producer = KafkaProducer(**config)
-fut = producer.send(topic=TOPIC, value=b"dan-message=1")
-
 # Consumer
 from kafka import KafkaConsumer
 consumer = KafkaConsumer(**config)
 consumer.subscribe(TOPIC)
-next(consumer)
+Thread(target=lambda: print(next(consumer))).start()
+
+
+# Producer
+from kafka import KafkaProducer
+producer = KafkaProducer(**config)
+fut = producer.send(topic=TOPIC, value=b"dan-message=1")
