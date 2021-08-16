1e7addcf53645874a2a3ed4c8ecc67ce36201742
Dan Davison <dandavison7@gmail.com>
2021-08-16 11:39:27 -0700

    Fix readers/writers

--- lbos_readers_writers.py
+++ lbos_readers_writers.py
@@ -1,53 +1,53 @@
+import random
 import time
 from typing import List
 from threading import Thread, Semaphore, Lock
 
 data = -1
 
-rw_mutex = Semaphore(1)
-readers_sem = Semaphore(0)
+mutex = Semaphore(1)
+room_empty = Semaphore(1)
 
-active_readers, blocked_readers = 0, 0
-ar_mutex = Semaphore(1)
+n_readers = 0
 
 
 def make_reader_thread(i: int):
     def closure():
-        with ar_mutex:
-            if active_readers == 0:
-                rw_mutex.acquire()
-                readers_sem.release(blocked_readers)
-
-            readers_sem.acquire()
-
-            active_readers += 1
+        global n_readers
+        with mutex:
+            n_readers += 1
+            if n_readers == 1:
+                room_empty.acquire()
 
         print(f"reader {i}: read data: {data}")
+        time.sleep(1)
 
-        with ar_mutex:
-            active_readers -= 1
-            if active_readers == 0:
-                rw_mutex.release()
+        with mutex:
+            n_readers -= 1
+            if n_readers == 0:
+                room_empty.release()
 
     return Thread(target=closure)
 
 
 def make_writer_thread(i: int):
     def closure():
-        rw_mutex.acquire()
         global data
+        room_empty.acquire()
         data = i
         print(f"writer {i}: data <- {data}")
-        rw_mutex.release()
+        time.sleep(1)
+        room_empty.release()
 
     return Thread(target=closure)
 
 
-n_readers, n_writers = 5, 3
+n_readers, n_writers = 15, 10
 readers = [make_reader_thread(i) for i in range(n_readers)]
 writers = [make_writer_thread(i) for i in range(n_writers)]
 
 threads = writers + readers
+random.shuffle(threads)
 
 for t in threads:
     t.start()
