96abaa762fdfdf439609d99c42862a77850cb678
Dan <dandavison7@gmail.com>
2022-03-04 16:51:03 -0500

    Update stash utilities

--- git-stash.sh
+++ git-stash.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-set -u
-STASH=/tmp/git-stash
+STASH_DIR=~/tmp/git-stash
+mkdir -p "$STASH_DIR"
 
 git-stash-save() {
-    git diff HEAD -- $SOURCE_PROJECT_DIR >$STASH
-    git apply -R <$STASH
+    local stash="$STASH_DIR/$1"
+    git diff HEAD -- $GIT_PATHS >"$stash"
+    git apply -R <"$stash"
 }
 
 git-stash-apply() {
-    git apply <$STASH
+    git apply <"$STASH_DIR/$1"
 }
 
 git-stash-show() {
-    cat $STASH
+    delta <"$STASH_DIR/$1"
+}
+
+git-diff() {
+    git diff $* -- $GIT_PATHS
 }
