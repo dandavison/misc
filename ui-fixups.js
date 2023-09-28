59bca6f2f1a23f4e8ab1550f4cb00f923b5c36d2
Dan Davison <dandavison7@gmail.com>
2023-09-28 05:38:02 -0400

    Web UI fixups

---
+++ ui-fixups.js
@@ -0,0 +1,5 @@
+// Change text color
+document.querySelectorAll('*').forEach(el => el.style.color = 'black')
+
+// ChatGPT increase code block widths
+document.querySelectorAll(".flex .items-start").forEach((el) => el.style.width = "1000px")
