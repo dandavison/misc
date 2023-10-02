5390a37d35f92a627cf6fc89f263cab1bb39edd5
Dan Davison <dandavison7@gmail.com>
2023-10-02 04:48:12 -0400

    Regex replace

--- ui-fixups.js
+++ ui-fixups.js
@@ -1,5 +1,13 @@
 // Change text color
 document.querySelectorAll('*').forEach(el => el.style.color = 'black')
 
+// Regex replace
+function replaceRegex(regex, replacement) {
+    document.body.innerHTML = document.body.innerHTML.replace(new RegExp(regex, 'gi'), replacement);
+}
+
+replaceRegex('what is \(?:a|an|the \)?\([^?]+\)\\?', '$1')
+
+
 // ChatGPT increase code block widths
 document.querySelectorAll(".flex .items-start").forEach((el) => el.style.width = "1000px")
